162183bf4177fd650905ad2dfd16289364ca965e
pamigit <pamizoki@gmail.com>
2019-09-18 13:28:57 +0200

    init commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Hello and well done!!!
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Todos"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Todos />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Todos from "../components/Todos";
+
+export default {
+  name: 'home',
+  components: {
+    Todos
+  }
+}
+</script>
